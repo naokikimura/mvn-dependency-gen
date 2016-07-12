d4be6c3e56615e37a93f9070bc5d9b98d310ed48
naokikimura <n.kimura.cap@gmail.com>
2016-07-13 01:46:10 +0900

    モジュール化しました。

---
+++ app.js
@@ -0,0 +1,81 @@
+const crypto = require('crypto');
+const debug = require('debug')('mvn-dependency-gen:app');
+const fs = require('fs');
+const http = require('http');
+const path = require('path');
+const querystring = require('querystring');
+const request = require('request-promise');
+const util = require('util');
+const xml2js = require('xml2js');
+
+module.exports.generate = line => {
+  debug(`Received: ${line}`);
+  const filename = line.trim();
+  digest(filename)
+    .then(digest => {
+      debug(`SHA1(${filename})= ${digest}`);
+      const query = { q: `1:"${digest}"`, rows: 20, wt: 'json' };
+      const baseurl = 'http://search.maven.org/solrsearch/select';
+      const url = `${baseurl}?${querystring.stringify(query)}`;
+      debug(`url: ${url}`);
+      return request({ uri: url, json: true });
+    })
+    .then(result => {
+      debug(`result: ${result}`);
+      const response = result.response || {};
+      debug(`numFound: ${response.numFound}`);
+      const dependencies = response.numFound
+        ? transformToDependency(response.docs)
+        : transformToSystemScopeDependency(filename);
+      debug(`dependencies: ${util.inspect(dependencies)}`);
+      const builder = new xml2js.Builder({
+        rootName: 'dependency',
+        headless: true,
+        renderOpts: { pretty: false }
+      });
+      dependencies.forEach(dependency => console.log(builder.buildObject(dependency)));
+    })
+    .catch(err => {
+      debug(util.inspect(err));
+      console.error(err.message);
+      process.exit(1);
+    });
+};
+
+function digest(filename, algorithm, encoding) {
+  return new Promise((resolve, reject) => {
+    const hash = crypto.createHash(algorithm || 'sha1');
+    fs.createReadStream(filename)
+      .on('end', () => resolve(hash.digest(encoding || 'hex')))
+      .on('error', reject)
+      .pipe(hash, { end: false });
+  });
+}
+module.exports.digest = digest;
+
+function transformToDependency(docs) {
+  return docs.map(doc => {
+    return {
+      groupId: [doc.g],
+      artifactId: [doc.a],
+      version: [doc.v]
+    }
+  });
+}
+module.exports.transformToDependency = transformToDependency;
+
+function transformToSystemScopeDependency(filename) {
+  const basename = path.basename(filename, '.jar');
+  const [artifactId, version] = basename.split(/-([^-]*$)/, 2);
+  const systemPath = path.isAbsolute(filename) ? filename : ['${basedir}', filename].join(path.sep);
+  return [
+    {
+      groupId: [artifactId],
+      artifactId: [artifactId],
+      version: [version || 'unknown'],
+      scope: ['system'],
+      systemPath: [systemPath]
+    }
+  ];
+}
+module.exports.transformToSystemScopeDependency = transformToSystemScopeDependency;
